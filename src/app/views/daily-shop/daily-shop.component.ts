f609ea4f45b7f15233a05d48aa890929ffc62706
Emilio Hernandez <emiliohernandezgt@gmail.com>
2021-03-15 11:34:50 -0600

    first commit based on mdbootstrap dashboard

---
+++ src/app/views/daily-shop/daily-shop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-shop',
+  templateUrl: './daily-shop.component.html',
+  styleUrls: ['./daily-shop.component.scss']
+})
+export class DailyShopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
